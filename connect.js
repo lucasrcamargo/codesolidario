d9c3f6c3b8637ccdb5cf76eec1aa48ff91cd1bcc
Daniele Reis <reis-daniele@hotmail.com>
2019-07-22 14:01:45 -0300

    config postgres2

--- connect.js
+++ connect.js
@@ -1,14 +1,9 @@
-const Pool = require('pg').Pool
+const Pool = require('pg');
 const pool = new Pool({
-  user: 'gliqhcjozleaiu',
-  host: 'lec2-174-129-41-127.compute-1.amazonaws.com',
-  database: 'ddu5cgfcnese5l',
-  password: '801b1654709e4e10e959846bec9ba4e30233b71f2986a8387baa02b668e6705b',
-  port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
 });
-pool.on('connect', () => {
-    console.log('connected to the db');
-});
+pool.connect();
 pool.query(
     'CREATE TABLE IF NOT EXISTS ideias(id SERIAL PRIMARY KEY, nome VARCHAR(40) not null, email VARCHAR(50) not null, categoria VARCHAR(40) not null, descricao VARCHAR(40) not null)');
   
