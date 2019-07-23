08119e086e0b858525d6dd7066795513b2400231
Daniele Reis <reis-daniele@hotmail.com>
2019-07-23 13:50:43 -0300

    melhorando o desempenho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, Output } from '@angular/core';
-declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,21 +6,4 @@ declare var $: any;
 })
 export class AppComponent {
   @Output() logged : boolean = false;
-  ngOnInit(): void {
-    $(document).ready(function(){
-      $(window).scroll(function(){
-          if ($(this).scrollTop() > 100) {
-              $('.move-top').fadeIn();
-          } else {
-              $('.move-top').fadeOut();
-          }
-      });
-  
-      $('.move-top').click(function(){
-          $('html, body').animate({scrollTop : 0},800);
-          return false;
-      });
-  });
-    
-  }
 }
