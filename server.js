acf3dfca3cfd2fc1d47920529ac32c1d476ea03e
Daniele Reis <reis-daniele@hotmail.com>
2019-07-23 13:20:36 -0300

    melhorando o desempenho

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/front'));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(require('prerender-node'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
